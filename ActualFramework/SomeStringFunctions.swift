79276bbe235dac60826be8119da613b6cc5d71a9
Vishwas Mukund <vishwas.mukund@mobileconsultingsolutions.com>
2020-01-02 13:46:45 -0500

    added new functionality

---
+++ ActualFramework/SomeStringFunctions.swift
@@ -0,0 +1,19 @@
+//
+//  SomeStringFunctions.swift
+//  ActualFramework
+//
+//  Created by MCS on 1/2/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+import UIKit
+
+public class SomeStringFunctions {
+    public static func capitalize(_ input: String) -> String {
+        return input.uppercased()
+    }
+    
+    public static func uncapitalize(_ input: String) -> String {
+        return input.lowercased()
+    }
+}
