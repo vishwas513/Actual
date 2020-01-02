952d97962a37fa2a68387e8c8932920c6a24beeb
Vishwas Mukund <vishwas.mukund@mobileconsultingsolutions.com>
2020-01-02 13:32:41 -0500

    Initial Commit

---
+++ ActualFramework/ActualFramework.h
@@ -0,0 +1,19 @@
+//
+//  ActualFramework.h
+//  ActualFramework
+//
+//  Created by MCS on 1/2/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ActualFramework.
+FOUNDATION_EXPORT double ActualFrameworkVersionNumber;
+
+//! Project version string for ActualFramework.
+FOUNDATION_EXPORT const unsigned char ActualFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActualFramework/PublicHeader.h>
+
+
